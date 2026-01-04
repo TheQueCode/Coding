34b88be45ca27669ba89a4fa8d0478f88b2e5f98
TheQueCode <thequecode@gmail.com>
2026-01-03 20:48:00 -0600

    Section 1 - Lesson 3

--- 01-variables-data-types/03-variables/script.js
+++ 01-variables-data-types/03-variables/script.js
@@ -1,12 +1,46 @@
 // Ways to declare a variable
 // var, let, const
 
+let firstName = 'John';
+let lastName = 'Doe';
+
+console.log(firstName, lastName);
+
+let age = 30;
+
+console.log(age);
+
+
 // Naming Conventions
 // - Only letters, numbers, underscores and dollar signs
 // - Can't start with a number
+
 
 // Multi-Word Formatting
 // firstName     camelCase
 // first_name    underscore
 // FirstName     PascalCase
-// firstname     owercase
+// firstname     lowercase
+
+// Re-assigning Variables
+
+age = 31;
+
+console.log(age);
+
+let score;
+
+score = 1;
+
+console.log(score);
+
+if(true){
+  score++;
+}
+
+console.log(score);
+
+const arr = [1,2,3,4];
+arr.push(5);
+
+console.log(arr);
