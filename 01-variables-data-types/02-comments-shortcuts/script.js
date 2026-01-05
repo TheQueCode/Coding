b0cea626e8e20d865794228d8c168a2064318321
TheQueCode <thequecode@gmail.com>
2026-01-05 14:33:52 -0600

    Comments and shortcuts

---
+++ 01-variables-data-types/02-comments-shortcuts/script.js
@@ -0,0 +1,84 @@
+// Ways to declare a variable
+// var, let, const
+
+let firstName = 'John';
+let lastName = 'Doe';
+
+console.log(firstName, lastName);
+
+let age = 30;
+
+console.log(age);
+
+
+// Naming Conventions
+// - Only letters, numbers, underscores and dollar signs
+// - Can't start with a number
+
+
+// Multi-Word Formatting
+// firstName     camelCase
+// first_name    underscore
+// FirstName     PascalCase
+// firstname     lowercase
+
+// Re-assigning Variables
+
+age = 31;
+
+console.log(age);
+
+let score;
+
+score = 1;
+
+console.log(score);
+
+if(true){
+  score++;
+}
+
+console.log(score);
+
+const arr = [1,2,3,4];
+arr.push(5);
+
+console.log(arr);
+
+// Explain things with comments
+// This is a single-line comment
+
+/*
+ This is a
+ multi-line comment
+*/
+
+// Shift + up/down arrow to copy line
+// Shift + right/left arrow to select by word
+// Alt + Left/Right arrow to go back/forward
+// Shift + Alt + up/down arrow to duplicate line
+// Alt + up/down arrow to move line up/down
+// Ctrl + / to comment/uncomment line
+// Ctrl + D to select next occurrence of selected word
+// Ctrl + Z to undo
+// Ctrl + Y to redo
+// Ctrl + X to cut line
+// Ctrl + C to copy line
+// Ctrl + V to paste line
+// Ctrl + Shift + K to delete line
+// Home to go to beginning of line
+// End to go to end of line
+// Ctrl + Home to go to beginning of file
+// Ctrl + End to go to end of file
+// Ctrl + F to find
+// Ctrl + H to replace
+// F2 to rename variable
+
+// Ctrl + B to toggle sidebar
+// Ctrl + ` to open terminal
+// Ctrl + Shift + N to open new window
+// Ctrl + W to close window
+// Ctrl + S to save file
+// Ctrl + Shift + S to save all files
+// Ctrl + P to open file
+// Ctrl + Shift + P to open command palette
