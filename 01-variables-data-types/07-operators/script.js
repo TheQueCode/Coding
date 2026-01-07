179980ecddad12785b2c5465cd2099394bbbd097
TheQueCode <thequecode@gmail.com>
2026-01-06 18:08:55 -0600

    Operators

--- 01-variables-data-types/07-operators/script.js
+++ 01-variables-data-types/07-operators/script.js
@@ -1,7 +1,55 @@
 // 1. Arithmetic Operators
 
+let x;
+
+x = 5 + 5;
+x = 5 - 5;
+x = 5 * 5;
+x = 5 / 5;
+x = 5 % 5;
+
+// Concatenation
+x = "Hello, " + "world!";
+
+// Exponent
+x = 5 ** 2;
+
+// Increment
+x = 1;
+x = x +1
+x++;
+
+// Decrement
+x = 1;
+x = x -1
+x--;
 
 // 2. Assignment Operators
 
+x = 10;
+
+x += 5;  // x = x + 5
+x -= 3;  // x = x - 3
+x *= 2;  // x = x * 2
+x /= 4;  // x = x / 4
+x %= 3;  // x = x % 3
+x **= 2; // x = x ** 2
 
 // 3. Comparison Operators
+
+x = 2 == '2';   // true
+x = 2 === '2';  // false
+x = 2 != 3;   // true
+x = 2 !== 3;  // true
+x = 2 > 1;    // true
+x = 2 < 3;    // true
+x = 2 >= 2;   // true
+x = 2 <= 3;   // true
+
+// 4. Logical Operators
+
+x = (5 > 3) && (2 < 4);
+x = (5 > 3) || (2 > 4);
+
+
+console.log(x);
