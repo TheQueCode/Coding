339091199e2f1c69b8bf0417f57a9ad8a69b336c
TheQueCode <thequecode@gmail.com>
2026-01-13 00:41:44 -0600

    Function challenge

--- 03-functions-scope/09-function-challenges/script.js
+++ 03-functions-scope/09-function-challenges/script.js
@@ -1,15 +1,12 @@
 // Challenge 1
-// function getCelsius(f) {
-//   const celsius = ((f - 32) * 5) / 9;
-//   return celsius;
-// }
 
-const getCelsius = (f) => ((f - 32) * 5) / 9;
+const getCelsius = fahrenheit => (fahrenheit - 32) * 5 / 9;
 
 console.log(`The temp is ${getCelsius(35)} \xB0C `);
 
 // Challenge 2
-function minMax(arr) {
+
+const minMax = arr => {
   const min = Math.min(...arr);
   const max = Math.max(...arr);
 
@@ -17,15 +14,18 @@ function minMax(arr) {
     min,
     max,
   };
-}
+};
 
 console.log(minMax([2, 31, 4, 5, 6]));
 
 // Challenge 3
-((length, width) => {
+
+const createObj = (length, width) => {
   const area = length * width;
 
   const output = `The area of a rectangle with a length of ${length} and a width of ${width} is ${area}.`;
 
   console.log(output);
-})(20, 10);
+};
+
+createObj(20, 10);
