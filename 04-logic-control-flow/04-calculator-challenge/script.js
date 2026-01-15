f8964d3f342dca413dcd7d9fdeeb9a6dc50ec984
TheQueCode <thequecode@gmail.com>
2026-01-15 17:18:56 -0600

    Calculator Challenge

--- 04-logic-control-flow/04-calculator-challenge/script.js
+++ 04-logic-control-flow/04-calculator-challenge/script.js
@@ -1,1 +1,25 @@
+function calculator(num1, num2, operator){
+  switch (operator) {
+    case '+':
+      console.log(num1 + num2);
+      break;
+    case '-':
+      console.log(num1 - num2);
+      break;
+    case '*':
+      console.log(num1 * num2);
+      break;
+    case '/':
+      console.log(num1 / num2);
+      break;
+    default:
+      console.log("Error");
+      break;
+  }
+}
 
+calculator(5, 2, '+');
+calculator(5, 2, '-');
+calculator(5, 2, '*');
+calculator(5, 2, '/');
+calculator(5, 2, '%');
