f5d1d6539559af7b61f271736dbda1e65b4116f5
TheQueCode <thequecode@gmail.com>
2026-01-17 20:17:58 -0600

    Events

--- 05-iteration-array-methods/01-for-loop/script.js
+++ 05-iteration-array-methods/01-for-loop/script.js
@@ -6,22 +6,22 @@
 // INCREMENT EXPRESSION - Expression that will be executed after each iteration of the loop. Usually increments the variable
 // STATEMENT - Code that will be executed each time the loop is run. To execute a `block` of code, use the `{}` syntax
 
-// for (let i = 0; i <= 10; i++) {
-//   if (i === 7) {
-//     console.log('7 is my lucky number');
-//   } else {
-//     console.log('Number ' + i);
-//   }
-// }
+for (let i = 0; i <= 10; i++) {
+  if (i === 7) {
+    console.log('7 is my lucky number');
+  } else {
+    console.log('Number ' + i);
+  }
+}
 
 // Nested loops
-// for (let i = 1; i <= 10; i++) {
-//   console.log('Number ' + i);
+for (let i = 1; i <= 10; i++) {
+  console.log('Number ' + i);
 
-//   for (let j = 1; j <= 5; j++) {
-//     console.log(`${i} * ${j} = ${i * j}`);
-//   }
-// }
+  for (let j = 1; j <= 5; j++) {
+    console.log(`${i} * ${j} = ${i * j}`);
+  }
+}
 
 // Loop through an array
 const names = ['Brad', 'Sam', 'Sara', 'John', 'Tim'];
@@ -33,3 +33,5 @@ for (let i = 0; i < names.length; i++) {
     console.log(names[i]);
   }
 }
+
+// for loop
